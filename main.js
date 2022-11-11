d1d22e5d5fe6610bf7ad30ad19ace3eca47b887b
LulzimHaziri <75541271+LulzimHaziri@users.noreply.github.com>
2022-11-12 00:19:55 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,57 @@
+//DETYRAT E SHTEPISE
+ //Request a user’s name and display the response “Hello, [name]”.
+      
+ //let username = prompt('enter username: ');
+    
+ //console.log('hello,', username );
+ // Request a user’s year of birth, count his age, and display the result. Store the current year as a constant.
+
+     //  let a= prompt('enter users year of birth: ')*1;
+      // let b= prompt('enter actual year: ')*1;
+      // let x= a - b;
+      // console.log(x);
+//Request a length of a side of a square from a user and display the perimeter of the square.
+    /*let a = prompt('length of a side of a square');
+
+    let x = a*4;
+     console.log(x); */
+    
+//Request a radius of a circle and display the area of such a circle.
+
+/*let r= prompt('enter circle radius');
+const Pi = 3.14;
+
+let x = 2*Pi*r;
+console.log(x);*/
+
+//Request a distance in km between the two cities from a user, and the time they want to cover it within. Count the speed needed to be on time.
+/*let distance = prompt('Enter the distance between cities: ') *1;
+       let time = prompt('Enter the time: ')*1;
+       console.log('The speed is ', distance/time, 'km/h');*/
+//Create a currency converter. A user types in USD and the program converts them to EUR. The exchange rate should be stored as a constant.
+
+
+ /* let a = prompt('Type a USD value: ')*1;
+ let x = a * 98 ;
+    
+        console.log(x , 'cents' );*/
+//A user states a flash drive memory in GB. The program has to count how many 820MB files can be stored on this flash drive.
+ 
+/*let a = prompt ('Type GB memory:')*1;
+
+let x = a * 1024 / 820 ;
+console.log(x,'files.');*/
+
+//A user types in an amount of money in a wallet and the price of one chocolate bar. The program counts how many chocolate bars the user can buy, and how much change will he/she have left.
+/*let a = prompt ('Type money that are in the wallet:')*1;
+
+let b = prompt ('Price of one chocolate bar:');
+let x = a-b;
+let y = a/b;
+console.log (x, '$ change after you buy one chocolate');
+console.log (y,'chocolates you can buy with your money');*/
+//The user types in a sum of bank deposits for 2 months with a yearly interest rate of 5%. The program counts the sum of interest.
+ let a = prompt('2 months deposit:');
+ let b = a * 0.83;
+ console.log ('sum of interest is:',a-b);
+
